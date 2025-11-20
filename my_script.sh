fafde0613a83f991ebf9830a33114b803d86ee59
punithmuthirevula <punithmuthirevula@gmail.com>
2025-11-20 07:51:31 +0530

    Changes for Bash script

---
+++ my_script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+ name ="hello"
+     if [ "$name" == "hello" ]; then
+            echo "Woed detected"
+            else
+                   echo "No Demo"
+                        fi
+                        
